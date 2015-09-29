9748c70ec9c6107cf62710117595bbc6906ca9b9
rphiteck <rphiteck@yahoo.com>
2015-09-29 18:22:18 -0400

    Prefabs

---
+++ Conquest/Assets/SpawnUnit.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnUnit : MonoBehaviour
+{
+    float currentCount;
+    public float maxUnitCount;
+    public float spawnTime;
+    public GameObject unit;
+    public Collider castleBoundaries;
+
+    void Awake()
+    {
+        castleBoundaries = GetComponent<Collider>();
+    }
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+    void Spawn()
+    {
+        if(currentCount >= maxUnitCount)
+        {
+            return;
+        }
+        Instantiate(unit, castle.position, castle.rotation);
+        currentCount++;
+    }
+}
