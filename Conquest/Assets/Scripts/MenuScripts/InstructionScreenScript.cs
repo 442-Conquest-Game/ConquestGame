ec94a438cf81c3981bdc7eb09b8145bfd6cae645
Christopher Dominessy <cdominessy@gmail.com>
2015-10-13 14:23:02 -0400

    Christopher
    Added Menu buttons to instruction and level select screen

---
+++ Conquest/Assets/Scripts/MenuScripts/InstructionScreenScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class InstructionScreenScript : MonoBehaviour {
+
+    public Canvas instructionMenu;
+    public Button menuButton;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        instructionMenu = instructionMenu.GetComponent<Canvas>();
+        menuButton = menuButton.GetComponent<Button>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void MenuButtonPress()
+    {
+        Application.LoadLevel(0);
+    }
+}
