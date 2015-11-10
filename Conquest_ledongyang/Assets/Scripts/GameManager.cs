b0eb6e686ae5d5c478e30d24fa10d0e6b53b3638
rphiteck <rphiteck@yahoo.com>
2015-11-10 13:46:17 -0500

    The game can now end and restart if the user chooses.

---
+++ Conquest_ledongyang/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public int redCount = 4;
+    public int blueCount = 4;
+    public GUIText endGameText;
+    public GUIText restartText;
+    public bool gameOver;
+    private bool restart;
+    // Use this for initialization
+    void Start () {
+        endGameText.text = "";
+        restartText.text = "";
+        gameOver = false;
+        restart = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameOver)
+        {
+            restartText.text = "Press 'R' to restart the game";
+            restart = true;
+        }
+	    if(restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+	}
+}
